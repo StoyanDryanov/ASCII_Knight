d67d5e847535d4382b5112b1d4f70b71e278772f
StoyanDryanov <stoyandryanov@gmail.com>
2026-01-05 20:58:50 +0200

    Place player

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -7,9 +7,15 @@
 using namespace std;
 
 // CONSTANTS
-const int ARENA_WIDTH = 80;
-const int ARENA_HEIGHT = 25;
+const int ARENA_WIDTH = 60;
+const int ARENA_HEIGHT = 20;
 const char WALL_CHAR = '#';
+const char PLAYER_CHAR = '@';
+
+struct Player {
+    int x;
+    int y;
+};
 
 void gotoXY(int x, int y)
 {
@@ -17,6 +23,11 @@ void gotoXY(int x, int y)
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 }
 
+void draw(int x, int y, char c) {
+    gotoXY(x, y);
+	cout << c;
+	gotoXY(0, ARENA_HEIGHT); // Move cursor out of the way
+}
 
 
 int main()
@@ -27,11 +38,14 @@ int main()
         {
             if (y == 0 || y == ARENA_HEIGHT - 1 || x == 0 || x == ARENA_WIDTH - 1)
             {
-				gotoXY(x, y);
-				cout << WALL_CHAR;
+				draw(x, y, WALL_CHAR);
             }
         }
     }
+
+	Player player = { ARENA_WIDTH / 2, ARENA_HEIGHT / 2 };
+
+	draw(player.x, player.y, PLAYER_CHAR);
 
     return 0;
 }
